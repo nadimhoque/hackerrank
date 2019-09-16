334be14eb622637f1c4c8d142c84b5c5411c13a8
Nadim Hoque <nadim@corneliouscarbon.localdomain>
2019-09-15 21:50:13 -0700

    The program checks if the string is a simple a and just returns n. If not it appends the string n number of times to the the temp array then counts the number of 'a' in the first n characters.

--- Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
+++ Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
@@ -9,22 +9,28 @@ import sys
 # Complete the repeatedString function below.
 def repeatedString(s, n):
     numa = 0
-    tmparray = []
+    temp = ''
 
-    for
+    if s == 'a':
+        return n
+    else:
+        for i in range(n):
+            temp = temp + s
 
-
+        for j in range(n):
+            if temp[j] == 'a':
+                numa = numa + 1
     return numa
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+   # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
     n = int(input())
 
     result = repeatedString(s, n)
+    print(result)
+    #fptr.write(str(result) + '\n')
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    #fptr.close()
