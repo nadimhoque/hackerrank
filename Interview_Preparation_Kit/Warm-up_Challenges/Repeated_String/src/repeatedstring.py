a426abec0197cff6689bee62913464fdcf6bc2dd
Nadim Hoque <nadim@corneliouscarbon.localdomain>
2019-10-07 00:49:28 -0700

    I am changing the algorithm since this fails of large number of repeats

--- Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
+++ Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
@@ -8,18 +8,8 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
-    numa = 0
-    temp = ''
+    
 
-    if s == 'a':
-        return n
-    else:
-        for i in range(n):
-            temp = temp + s
-
-        for j in range(n):
-            if temp[j] == 'a':
-                numa = numa + 1
     return numa
 
 if __name__ == '__main__':
