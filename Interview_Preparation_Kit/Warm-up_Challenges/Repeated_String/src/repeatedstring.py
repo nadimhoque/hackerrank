ab4dc5a8c612fdb554bc2b3af7ef2c8afd0c0164
Nadim Hoque <nadim@corneliouscarbon.localdomain>
2019-10-07 01:23:19 -0700

    All of the test cases passed on hacker rank

--- Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
+++ Interview_Preparation_Kit/Warm-up_Challenges/Repeated_String/src/repeatedstring.py
@@ -9,8 +9,25 @@ import sys
 # Complete the repeatedString function below.
 def repeatedString(s, n):
     
+    numa = 0
+    suba = 0
 
-    return numa
+    for i in range(len(s)):
+        if s[i] == 'a':
+            suba = suba + 1
+
+    if s == 'a':
+        numa = n
+    else:
+        if n % len(s) == 0:
+            numa = suba * (n/len(s))
+        else:
+            numa = suba * math.floor(n/len(s))
+            for i in range(n%len(s)):
+                if s[i] == 'a':
+                    numa = numa +1
+
+    return math.floor(numa)
 
 if __name__ == '__main__':
    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
