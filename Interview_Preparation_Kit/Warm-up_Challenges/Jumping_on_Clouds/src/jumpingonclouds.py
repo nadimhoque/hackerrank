d864f03baca235db152b0ac98a99476ea3b17747
nadimhoque <nadimhoque@gmail.com>
2020-08-06 23:52:23 -0700

    Update jumpingonclouds.py

--- Interview_Preparation_Kit/Warm-up_Challenges/Jumping_on_Clouds/src/jumpingonclouds.py
+++ Interview_Preparation_Kit/Warm-up_Challenges/Jumping_on_Clouds/src/jumpingonclouds.py
@@ -8,8 +8,21 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    numjump=0
+    jump=0
+    size=len(c) -1
+    while (jump <= size):
+        if  (jump+2) < size and c[jump+2] == 0:
+            jump=jump+2
+            numjump = numjump+1
+        elif c[jump+1] == 0:
+            jump=jump+1
+            numjump = numjump+1     
+    print(numjump)
 
+"""
 if __name__ == '__main__':
+    main()
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
@@ -21,3 +34,10 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+    """
+n = int(input())
+c = list(map(int, input().rstrip().split()))
+jumpingOnClouds(c)
+#result = jumpingOnClouds(c)
+#print(result)
+
