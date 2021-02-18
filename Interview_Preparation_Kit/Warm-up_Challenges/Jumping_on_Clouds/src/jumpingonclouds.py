80487814671fc00ef9fb386655d171df255ca02f
Nadim Hoque <nadimhoque@gmail.com>
2021-02-18 00:38:29 -0800

    I fixed the bounds issues within the if/else statement.

--- Interview_Preparation_Kit/Warm-up_Challenges/Jumping_on_Clouds/src/jumpingonclouds.py
+++ Interview_Preparation_Kit/Warm-up_Challenges/Jumping_on_Clouds/src/jumpingonclouds.py
@@ -1,43 +1,21 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
-    numjump=0
-    jump=0
-    size=len(c) -1
-    while (jump <= size):
-        if  (jump+2) < size and c[jump+2] == 0:
-            jump=jump+2
+    numjump = 0
+    jump = 0
+    size = len(c) -1
+    while jump <= size:
+        if (jump+2) < size and c[jump+2] == 0:
+            jump = jump + 2
+            numjump = numjump + 1
+        elif c[jump+1] == 0 and (jump+1) < size:
+            jump = jump + 1
             numjump = numjump+1
-        elif c[jump+1] == 0:
-            jump=jump+1
-            numjump = numjump+1     
-    print(numjump)
+    return numjump
 
-"""
-if __name__ == '__main__':
-    main()
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input())
+test = [0, 0, 1, 0, 0, 1, 0]
 
-    c = list(map(int, input().rstrip().split()))
-
-    result = jumpingOnClouds(c)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
-    """
-n = int(input())
-c = list(map(int, input().rstrip().split()))
-jumpingOnClouds(c)
-#result = jumpingOnClouds(c)
-#print(result)
-
+may=jumpingOnClouds(test)
+print(may)
