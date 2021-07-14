c405596bf94fc5faa15bf7f397a832153001caa6
Nadim Hoque <nadim@corneliousnuc.localdomain>
2021-07-14 08:59:42 -0700

    The code works on hacker rank, but might be written a bit better

--- Python Challenges/nested_lists/nestedlist.py
+++ Python Challenges/nested_lists/nestedlist.py
@@ -1,12 +1,29 @@
 if __name__ == '__main__':
     #now create a empty dictionary
     sheet={}
+    
     #the for loop will add the names and score to the dictionary with the name as the key and the score as the value
     for _ in range(int(input())):
         name = input()
         score = float(input())
         sheet[name]=score
-    #created another list 
+    
+    #created another list that contains just the grades
     values=sheet.values()
+    
+    #I am going to convert the list to a set so that all duplicates are removed. Then I will sort this list so it is in ascending order. Because
+    #there are nore repeats I can assume that the second item is the second largest number
     secondsmallest=sorted(set(values))[1]
-    print(secondsmallest)
+
+    #create a empty list so that we can sort it later
+    names = []
+    #go through the dictionary. if the value matches the second highest score, append it to the names list
+    for key,value in sheet.items():
+        if value == secondsmallest:
+            names.append(key)
+    
+    #sort the names list so the names are alphabetical
+    names.sort()
+    #the for loop then prints all of the names one line at a time
+    for i in names:
+        print(i)
