ca997fa44c83349fb62b9179582db0c6c11e2cd9
Nadim Hoque <nadim@corneliousnuc.localdomain>
2021-07-14 14:15:16 -0700

    code complete

--- Python Challenges/string_validators/stringvalidators.py
+++ Python Challenges/string_validators/stringvalidators.py
@@ -1,8 +1,9 @@
 if __name__ == '__main__':
+    
     s = input()
-
-    print(s.isalnum())
-    print(s.isalpha())
-    print(s.isdigit())
-    print(s.islower())
-    print(s.isupper())
+    #I used the any returns true if any of the is* are true in the string
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
