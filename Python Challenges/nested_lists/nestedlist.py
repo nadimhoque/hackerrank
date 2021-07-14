f905e4ea11ce214b54b44c6ab33d618c5a7242f1
Nadim Hoque <nadim@corneliousnuc.localdomain>
2021-07-13 22:26:27 -0700

    I have started on the program. I first copied what hackerrank had and added some code. So far I have created a dictionary (key-value pairs) that contains the student and their score. Then I created another list that contains a set of all of the scores, then that is sorted in assending order. The next step is to go through the dictionary and print all of the names that are associated with the second highest score.
    I also want to note the print statements are for my debugging purposes
    only.

--- Python Challenges/nested_lists/nestedlist.py
+++ Python Challenges/nested_lists/nestedlist.py
@@ -1,4 +1,12 @@
 if __name__ == '__main__':
+    #now create a empty dictionary
+    sheet={}
+    #the for loop will add the names and score to the dictionary with the name as the key and the score as the value
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        sheet[name]=score
+    #created another list 
+    values=sheet.values()
+    secondsmallest=sorted(set(values))[1]
+    print(secondsmallest)
