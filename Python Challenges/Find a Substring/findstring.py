890a2330b5b6ba18150d7459d92b0b1655095e6d
Nadim Hoque <nadim@corneliousnuc.localdomain>
2021-07-14 11:56:06 -0700

    Working code, but I had some help with this one

--- Python Challenges/Find a Substring/findstring.py
+++ Python Challenges/Find a Substring/findstring.py
@@ -1,16 +1,12 @@
 def count_substring(string, sub_string):
-    mainstr=list(string)
-    substr=list(sub_string)
     nummatch = 0
 
-    for itrstr in range(0,len(mainstr)):
-        if substr[0] == mainstr[itrstr]:
-            itrsub = 0
-            for itrsub in range(0, len(substr)):
-                if substr[itrstr] != mainstr[itrsub+itrstr] and itrstr + itrsub:
-                    break
-                else:
-                    nummatch = nummatch + 1
+    for itrstr in range(0,len(string)):
+        if string[i:].startswith(substring):
+            nummatch += 1
+
+    return nummatch
+        
 
 if __name__ == '__main__':
     string = input().strip()
