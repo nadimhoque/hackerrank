96e3053662cafee76602429eb751334968a2d0e9
Nadim Hoque <nadim@corneliousnuc.localdomain>
2021-07-14 10:25:14 -0700

    The findthepercentage.py is complete and works

--- Python Challenges/finding_the_percentage/findthepercentage.py
+++ Python Challenges/finding_the_percentage/findthepercentage.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+
+    #I have put the grades of the select user into the list called grades
+    grades = student_marks[query_name]
+    
+    #I used the sum() to have python add all the contents of the grades list, then I divided by the length of the list. Technically I could have used 3 but this code is a bit more flexible especially because the input has no restriction on how many the user can enter for the grades
+    avg=sum(grades)/len(grades)
+
+    print(format(round(avg,2),".2f"))
